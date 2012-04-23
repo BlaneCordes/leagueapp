77d9892d4b5292cc7265083e1b2f532ca7fb855a
Blane <blane.cordes@gmail.com>
2012-04-23 00:29:17 -0400

    added players to teams index

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,23 @@
 class Team < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :team_id
   
   has_many :players 
   
   has_many :home_games, :class_name => Game, :foreign_key => :home_team_id
   has_many :away_games, :class_name => Game, :foreign_key => :away_game_id
+  
+ def team_players(team_id)
+   x = Player.find_all_by_team_id(team_id)
+   x.each do |player|
+     player.name
+   end
+ end
+ 
+ def find_players_names
+   self.players.map do |x|
+	   x.name
+	end
+ end
+    
+    
 end
