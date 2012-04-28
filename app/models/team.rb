5c1c0514d012f1a7d1d0ac5ecc5194d1a89f48a9
Blane <blane.cordes@gmail.com>
2012-04-28 09:59:43 -0400

    restarting

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,18 +6,5 @@ class Team < ActiveRecord::Base
   has_many :home_games, :class_name => Game, :foreign_key => :home_team_id
   has_many :away_games, :class_name => Game, :foreign_key => :away_game_id
   
- def team_players(team_id)
-   x = Player.find_all_by_team_id(team_id)
-   x.each do |player|
-     player.name
-   end
- end
- 
- def find_players_names
-   self.players.map do |x|
-	   x.name
-	end
- end
-    
     
 end
