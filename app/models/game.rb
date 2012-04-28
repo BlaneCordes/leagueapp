57e3a1b58e39d6b870feb237e8e2a942dc8ba61a
Blane <blane.cordes@gmail.com>
2012-04-28 11:51:42 -0400

    clean up

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,16 +4,16 @@ class Game < ActiveRecord::Base
   belongs_to :home_team, :class_name => Team
   belongs_to :away_team, :class_name => Team
   
-  def home_wins(game)
-    	#if home_score is greater than away_score
-    	if game.home_score > game.away_score
-    	#add 1 to an array
-    	  a = [] << 1
-    	#sum up the array for home wins
-    	puts a.count
-      end
-    end
-  end
+  # def home_wins(game)
+  #     #if home_score is greater than away_score
+  #     if game.home_score > game.away_score
+  #     #add 1 to an array
+  #       a = [] << 1
+  #     #sum up the array for home wins
+  #     puts a.count
+  #     end
+  #   end
+  # end
 
 end
 
