d92a88d3e5b838323546887e9a47a2b4a18cf765
Blane <blane.cordes@gmail.com>
2012-05-28 22:34:37 -0400

    fixed new boxscore path

--- app/models/boxscore.rb
+++ app/models/boxscore.rb
@@ -1,12 +1,24 @@
 class Boxscore < ActiveRecord::Base
   attr_accessible :boxscore_id, :assists, :blocks, :fgattempted, :fgmade, :game_id, :player_id, :points, :rebounds, :steals, :threepointattempted, :threepointmade, :turnovers
   
+  validates :player_id, :presence => true
+  
   belongs_to :game 
   belongs_to :player 
   
   #TODO need to make this method work when the boxscore player ID is nil
-  def player_name(player_id)
-    player = Player.find_by_id(player_id)
-    player.name
+  def player_name_id
+    boxscore.player_id
   end
+  
+  def player_name(player_id)
+  x = Player.find_by_id(player_id)
+  if x.name.present?
+  x.name
+  else
+      puts "Blank player name"
+  end
+end
+  
+  
 end
