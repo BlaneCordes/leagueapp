d3319d19225cd58573ac3e5ddbe368a8ed306233
Blane <blane.cordes@gmail.com>
2012-05-31 19:44:06 -0400

    view boxscore link working

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,8 @@ class Player < ActiveRecord::Base
   attr_accessible :name, :player_id, :team_id, :email
   has_many :boxscores
   
+  has_many :games, :through => :home_team, :class_name => Team
+  
   belongs_to :team
 
 
